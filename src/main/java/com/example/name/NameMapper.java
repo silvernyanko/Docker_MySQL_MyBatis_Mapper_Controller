a5cdfdbd8dc0fbb16277313167f5d18ec8ff362a
silvernyanko <h8octf.jay.mcbz@gmail.com>
2023-12-09 22:07:05 +0900

    docker、Mapper、エンティティ定義と設定

---
+++ src/main/java/com/example/name/NameMapper.java
@@ -0,0 +1,13 @@
+package com.example.name;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
+
+@Mapper
+public interface NameMapper {
+    @Select("SELECT * FROM names")
+    List<Name> findAll();
+
+}
