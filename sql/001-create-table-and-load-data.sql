30c3203b67c441a4aad56dab45592f35039f43b0
silvernyanko <h8octf.jay.mcbz@gmail.com>
2023-12-11 17:22:47 +0900

    NameController追加、VALUES追加・変更

--- sql/001-create-table-and-load-data.sql
+++ sql/001-create-table-and-load-data.sql
@@ -6,5 +6,15 @@ CREATE TABLE names (
     PRIMARY KEY(id)
 );
 
-INSERT INTO names (name) VALUES ('name1');
-INSERT INTO names (name) VALUES ('name2');
+INSERT INTO names (name) VALUES ('横須賀高校');
+INSERT INTO names (name) VALUES ('追浜高校');
+INSERT INTO names (name) VALUES ('横須賀大津高校');
+INSERT INTO names (name) VALUES ('横須賀総合高校');
+INSERT INTO names (name) VALUES ('横須賀南高校');
+INSERT INTO names (name) VALUES ('津久井浜高校');
+INSERT INTO names (name) VALUES ('海洋科学高校');
+INSERT INTO names (name) VALUES ('横須賀工業高校');
+INSERT INTO names (name) VALUES ('横須賀学院高校');
+INSERT INTO names (name) VALUES ('緑ヶ丘女子高校');
+INSERT INTO names (name) VALUES ('三浦学苑高校');
+INSERT INTO names (name) VALUES ('湘南学院高校');
